3bf757dd0c2c12ad4a9946904715a7154a6d369c
Heng Vina <145192232+vinaheng@users.noreply.github.com>
2023-11-14 16:37:32 +0700

    thrid commit

--- components/AppHeader/index.vue
+++ components/AppHeader/index.vue
@@ -1,14 +1,16 @@
 <template>
     <transition appear name="header">
         <div v-show="true" class="container m-auto flex items-center text-header justify-between px-4 relative">
-            <!-- <div id="bar" class="absolute z-30 top-0"></div> -->
             <NuxtLink to="/"><img src="~/assets/img/Blog_pic.svg" class="h-14" alt="" /></NuxtLink>
             <div class="flex gap-10">
                 <Menu class="hidden xl:flex" />
                 <div class="gap-2 hidden xl:flex">
                     <SwitchLanguage />
                     <DarkMode />
-                    <Button class="bg-blue-900 text-white rounded-sm py-2 px-4 gap-6">{{ $t('login') }}</Button>
+                    <Button
+                        class="bg-blue-800 border-[1px] dark:border-slate-700 text-white rounded-md py-2 px-4 gap-6"
+                        >{{ $t('login') }}</Button
+                    >
                 </div>
             </div>
             <!-- Menu Mobile -->
@@ -34,24 +36,4 @@
 </template>
 <script setup lang="ts">
 const { menuState, toggleMenu } = useMenu();
-// let scr = 0;
-// let progress = 0;
-// const fullHeight = ref(document.body.scrollHeight - window.innerHeight);
-// const handleScroll = () => {
-//     scr = window.scrollY;
-//     progress = (scr / fullHeight.value) * 100;
-//     document.getElementById('bar').style.width = `${progress}vw`;
-// };
-
-// onMounted(() => {
-//     window.addEventListener('scroll', handleScroll);
-// });
-
-// onUnmounted(() => {
-//     window.removeEventListener('scroll', handleScroll);
-// });
-const title = ref('Lesson');
-useSeoMeta({
-    title,
-});
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
         <div
             class="w-full fixed top-0 border-b-2 z-10 bg-white dark:bg-slate-900 dark:text-white dark:border-slate-700"
         >
+            <div class="absolute z-30 top-0 scroll-indicator scroll-indicator"></div>
             <AppHeader />
         </div>
         <slot />
