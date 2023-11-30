0c34c2d7fb41e4748a218f4583e9109e3f1e19ca
Heng Vina <145192232+vinaheng@users.noreply.github.com>
2023-11-30 23:05:00 +0700

    fist commit

--- pages/about_us/index.vue
+++ pages/about_us/index.vue
@@ -5,5 +5,19 @@ useSeoMeta({
 });
 </script>
 <template>
-    <Test />
+    <transition appear name="page">
+        <div v-show="true" class="max-w-[700px] m-auto p-5 pt-20 mt-14 dark:text-white">
+            <div class="flex flex-col items-center">
+                <h2 class="lg:text-6xl text-4xl">{{ $t('about_us') }}</h2>
+                <span class="m-8 dark:border-slate-700 border w-full"></span>
+                <h2 class="text-2xl lg:text-3xl">"{{ $t('why_girl_hate_me') }}"</h2>
+                <hr class="m-8 dark:border-slate-700" />
+                <img class="object-cover rounded-md shadow-md" src="~/assets/img/use_admin.jpg" alt="" />
+            </div>
+            <div class="my-6">
+                <h2 class="lg:text-2xl text-xl my-8">{{ $t('about_title') }}</h2>
+                <span class="text-xl font_san_200">{{ $t('about_one') }}</span>
+            </div>
+        </div>
+    </transition>
 </template>
